fb8be5690d602ff817d3b769fda29415db142d93
rupert <rupert@2rmobile.com>
2014-04-06 13:18:28 +1000

    Change Google Maps to MapBox.  Use geojson json object for markers

--- data.js
+++ data.js
@@ -1,60 +1,100 @@
 var map, marker_bounds;
 
-var experiences = [
+var geojson = [
   {
-    type: "Experience",
-    date_to: "now",
-    date_from: "07/25/2011",
-    location: "Melbourne, Australia",
-    title: "Software Developer",
-    company: "Datalink Technologies Pty Ltd",
-    latitude: -37.926672,
-    longitude: 145.228883,
-    description: "Develop proof of concept mobile applications to support underground asset management for Dial Before You Dig. Technologies include ObjectiveC, C# and Java, Oracle Spatial, Postgres/Postgis, Mapserver and Geoserver. Integration with Google, Bing Maps and deCarta (Java). Spatial analysis (i.e spatial intersections) and geometry transformation using FME."
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [145.228883,-37.926672]
+    },
+    "properties": {
+      "marker-color": "#D73C50",
+      "marker-size": "large",
+      "marker-symbol": "car",
+      category: "Experience",
+      date_to: "now",
+      date_from: "07/25/2011",
+      location: "Melbourne, Australia",
+      title: "Software Developer",
+      company: "Datalink Technologies Pty Ltd",
+      description: "Develop proof of concept mobile applications to support underground asset management for Dial Before You Dig. Technologies include ObjectiveC, C# and Java, Oracle Spatial, Postgres/Postgis, Mapserver and Geoserver. Integration with Google, Bing Maps and deCarta (Java). Spatial analysis (i.e spatial intersections) and geometry transformation using FME."
+    }
   },
   {
-    type: "Experience",
-    date_to: "07/01/2011",
-    date_from: "05/01/2011",
-    location: "Melbourne, Australia",
-    title: "iPhone Developer",
-    company: "PlayUp / Revo Pty Ltd",
-    latitude: -37.837833,
-    longitude: 144.97652500000004,
-    description: "Develop proof of concept mobile applications to support underground asset management for Dial Before You Dig. Technologies include ObjectiveC, C# and Java, Oracle Spatial, Postgres/Postgis, Mapserver and Geoserver. Integration with Google, Bing Maps and deCarta (Java). Spatial analysis (i.e spatial intersections) and geometry transformation using FME."
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [144.97652500000004, -37.837833]
+    },
+    "properties": {
+      "marker-color": "#0A2628",
+      "marker-size": "large",
+      "marker-symbol": "city",
+      category: "Experience",
+      date_to: "07/01/2011",
+      date_from: "05/01/2011",
+      location: "Melbourne, Australia",
+      title: "iPhone Developer",
+      company: "PlayUp / Revo Pty Ltd",
+      description: "Develop proof of concept mobile applications to support underground asset management for Dial Before You Dig. Technologies include ObjectiveC, C# and Java, Oracle Spatial, Postgres/Postgis, Mapserver and Geoserver. Integration with Google, Bing Maps and deCarta (Java). Spatial analysis (i.e spatial intersections) and geometry transformation using FME."
+    }
   },
   {
-    type: "Experience",
-    date_to: "05/01/2011",
-    date_from: "05/01/2009",
-    location: "Melbourne, Australia",
-    title: "Mobile Developer",
-    company: "PelicanCorp Pty Ltd",
-    latitude: -37.806503,
-    longitude: 144.987975,
-    description: "Develop proof of concept mobile applications to support underground asset management for Dial Before You Dig. Technologies include ObjectiveC, C# and Java, Oracle Spatial, Postgres/Postgis, Mapserver and Geoserver. Integration with Google, Bing Maps and deCarta (Java). Spatial analysis (i.e spatial intersections) and geometry transformation using FME."
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [144.987975, -37.806503]
+    },
+    "properties": {
+      "marker-color": "#479C8C",
+      "marker-size": "large",
+      "marker-symbol": "america-football",
+      category: "Experience",
+      date_to: "05/01/2011",
+      date_from: "05/01/2009",
+      location: "Melbourne, Australia",
+      title: "Mobile Developer",
+      company: "PelicanCorp Pty Ltd",
+      description: "Develop proof of concept mobile applications to support underground asset management for Dial Before You Dig. Technologies include ObjectiveC, C# and Java, Oracle Spatial, Postgres/Postgis, Mapserver and Geoserver. Integration with Google, Bing Maps and deCarta (Java). Spatial analysis (i.e spatial intersections) and geometry transformation using FME."
+    }
   },
   {
-    type: "Experience",
-    date_to: "01/02/2009",
-    date_from: "11/01/2006",
-    location: "Beijing, China",
-    title: "Team Lead / Tech Lead",
-    company: "Cybersoft Beijing Information Technology",
-    latitude: 39.90403,
-    longitude: 116.40752599999996,
-    description: "Cybersoft Beijing is involved in providing mobile travel and mapping systems to telecom providers in China during the Beijing 2008 Olympics."
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [116.40752599999996, 39.90403]
+    },
+    "properties": {
+      "marker-color": "#6EB4A4",
+      "marker-size": "large",
+      "marker-symbol": "suitcase",
+      category: "Experience",
+      date_to: "01/02/2009",
+      date_from: "11/01/2006",
+      location: "Beijing, China",
+      title: "Team Lead / Tech Lead",
+      company: "Cybersoft Beijing Information Technology",
+      description: "Cybersoft Beijing is involved in providing mobile travel and mapping systems to telecom providers in China during the Beijing 2008 Olympics."
+    }
   },
   {
-    type: "Experience",
-    date_to: "2006/10/01",
-    date_from: "2004/05/01",
-    location: "Manila, Philippines",
-    title: "Systems Admin / Lead Developer",
-    company: "Cybersoft Integrated Geoinformatics Inc",
-    latitude: 14.639163,
-    longitude: 121.03002500000002,
-    description: "Cybersoft GeoInformatics is responsible in providing GIS data and mapping capabilities to DPC YellowPages, Mapcentral Philippines and Telecom Providers. It also has extensive experience in vehicle asset tracking and monitoring thru PLDT."
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [121.03002500000002, 14.639163]
+    },
+    "properties": {
+      "marker-color": "#98CAB7",
+      "marker-size": "large",
+      "marker-symbol": "mobilephone",
+      category: "Experience",
+      date_to: "2006/10/01",
+      date_from: "2004/05/01",
+      location: "Manila, Philippines",
+      title: "Systems Admin / Lead Developer",
+      company: "Cybersoft Integrated Geoinformatics Inc",
+      description: "Cybersoft GeoInformatics is responsible in providing GIS data and mapping capabilities to DPC YellowPages, Mapcentral Philippines and Telecom Providers. It also has extensive experience in vehicle asset tracking and monitoring thru PLDT."
+    }
   }
 ];
 
@@ -100,8 +140,8 @@ function getDuration(dtStart, dtEnd){
 }
 
 function createExperiences(){
-  for(var i in experiences) {
-    var ex = experiences[i];
+  for(var i in geojson) {
+    var ex = geojson[i].properties;
     var html;
     html  = "<div class='row'>";
     html += "  <div class='col-md-4'>";
@@ -120,47 +160,12 @@ function createExperiences(){
 }
 
 function initMap() {
-  var mapOptions = {
-    center: new google.maps.LatLng(-34.397, 150.644),
-    zoom: 8
-  };
+  map = L.mapbox.map('map-canvas', 'rupert.hnb5c3da');
 
-  markers_bounds = new google.maps.LatLngBounds();
+  var featureLayer = L.mapbox.featureLayer(geojson).addTo(map);
 
-  map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-
-  for(var i in experiences) {
-    var data = experiences[i];
-    createMarker(data);
-  }
-}
-
-function createMarker(data){
-  var myLatlng = new google.maps.LatLng(data.latitude, data.longitude);
-
-  var marker = new google.maps.Marker({
-        position: myLatlng,
-        map: map,
-        title: map.title + ' - ' + map.company
-  });
-
-  var contentString;
-  contentString =  "<div>";
-  contentString += "  <strong>" + data.title +"</strong> <br/>";
-  contentString += "  <strong>" + data.company +"</strong> <br/>";
-  contentString += "  <p>" + data.description +"</p>";
-  contentString += "</div>";
-
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString
-  });
-
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.open(map,marker);
-  });
-
-  markers_bounds.extend(myLatlng);
-  map.fitBounds(markers_bounds);
+  map.fitBounds(featureLayer.getBounds());
+  map.setZoom(3);
 }
 
 $( document ).ready(function() {

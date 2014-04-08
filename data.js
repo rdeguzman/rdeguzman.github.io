14c04481623fc2bef0b716dc9045c5a002628b43
rupert <rupert@2rmobile.com>
2014-04-09 00:59:06 +1000

    Switch to horizontal bar chart and similar data structure to tsv (tab separated values)

--- data.js
+++ data.js
@@ -282,42 +282,40 @@ function round(value, places) {
 
 function initChart(){
   var data = [];
-  var keys = [];
 
   var hashObj = getData();
   for(var key in hashObj){
-    keys.push(key);
-    data.push(hashObj[key]);
+    var d = {};
+    d.name = key;
+    d.value = hashObj[key];
+    data.push(d);
   }
 
-  var width = 1080,
-      height = 300;
+  var width = 500,
+      barHeight = 20;
 
   var chart = d3.select(".chart")
       .attr("width", width)
-      .attr("height", height);
+      .attr("height", barHeight * data.length);
 
-  var y = d3.scale.linear()
-      .domain([0, d3.max(data)])
-      .range([height, 0]);
-
-  var barWidth = width / data.length;
+  var x = d3.scale.linear()
+      .domain([0, d3.max(data, function(d) { return d.value; })])
+      .range([0, width]);
 
   var bar = chart.selectAll("g")
       .data(data)
     .enter().append("g")
-      .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; });
+      .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
 
   bar.append("rect")
-        .attr("y", function(d) { return y(d); })
-        .attr("height", function(d) { return height - y(d); })
-        .attr("width", barWidth - 1);
+      .attr("width", function(d) { return x(d.value); })
+      .attr("height", barHeight - 1);
 
   bar.append("text")
-      .attr("x", barWidth / 2)
-      .attr("y", function(d) { return y(d) + 3; })
-      .attr("dy", ".75em")
-      .text(function(d) { return d; });
+      .attr("x", function(d) { return x(d.value) - 3; })
+      .attr("y", barHeight / 2)
+      .attr("dy", ".35em")
+      .text(function(d) { return d.name + " " + d.value; });
 }
 
 $( document ).ready(function() {

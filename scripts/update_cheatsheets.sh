136a6baa04836be7903d7c30c5aca74d843b98f1
rupert <rupert@2rmobile.com>
2015-04-02 01:39:04 +1100

    Update cheatsheet script to automatically push

--- scripts/update_cheatsheets.sh
+++ scripts/update_cheatsheets.sh
@@ -1,1 +1,11 @@
+#!/bin/sh
 git submodule update --remote cheatsheets
+
+# True if the length of string is zero.
+if [ -z "$1" ]; then
+  message="Update cheatsheets"
+else
+  message="$1"
+fi
+
+git commit -m "$message" cheatsheets
